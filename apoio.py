67ad8af67c2eb2dfffb65068874ce71e82a30bec
Fabio Lara <fabiolc.lara@gmail.com>
2021-11-12 06:41:29 -0300

    alteracao .01

--- apoio.py
+++
@@ -1,9 +0,0 @@
-def senha():
-    password = 'apm'
-    digitacao = input('Digite a senha do sistema:')
-    if password == digitacao:
-        def escola():
-            return render_template('escola.html')
-    else:
-        def retorno():
-            return render_template('index.html')
